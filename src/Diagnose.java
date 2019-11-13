5b7af1e183abed326714f1fd0a3dc6b8da5ce1ec
JamesTD1949 <jamestdowning27@gmail.com>
2019-11-13 10:43:21 +0000

    End of morning class 13/11

---
+++ src/Diagnose.java
@@ -0,0 +1,64 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Diagnose {
+    private static int id;
+    private String condition,date;
+    private int petID,severity;
+    private Date today;
+
+    public Diagnose(String condition, int petID, int severity)
+    {
+        setCondition(condition);
+        setPetID(petID);
+        setSeverity(severity);
+        Date today = new Date();
+
+        SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("dd-MM-yyyy");
+        date = DATE_FORMAT.format(today);
+
+        setDate(date);
+
+        id++;
+    }
+
+    public static int getId() {
+        return id;
+    }
+
+    public String getCondition() {
+        return condition;
+    }
+
+    public void setCondition(String condition) {
+        this.condition = condition;
+    }
+
+    public int getPetID() {
+        return petID;
+    }
+
+    public void setPetID(int petID) {
+        this.petID = petID;
+    }
+
+    public int getSeverity() {
+        return severity;
+    }
+
+    public void setSeverity(int severity) {
+        this.severity = severity;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String toString() {
+        return "Diagnosis ID: " + getId() + " Condition: " + getCondition() + " Severity: " + getSeverity() + " Pet ID: " + getPetID() + " Date of Diagnosis: " + getDate();
+    }
+}
---
+++ src/DiagnoseDriver.java
@@ -0,0 +1,10 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DiagnoseDriver {
+    public static void main(String[] args) {
+        Diagnose test = new Diagnose("Kennel Cough",33,4);
+
+        System.out.println(test.toString());
+    }
+}
